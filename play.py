9c77359b7a4e099ebfcf644ecd3c23a3abb5d0f7
hbusul <h.burak.usul@gmail.com>
2021-10-02 17:55:47 +0300

    add option to test after training, also add option to use a model while training

--- play.py
+++ play.py
@@ -1,5 +1,6 @@
 import argparse
 import pickle
+import sys
 
 import gym
 import numpy as np
@@ -62,8 +63,46 @@ def q(values, state: np.ndarray):
         values[x] = [np.array([0.2] * 9), np.array([0] * 9)]
     return values[x]
 
+def update_q(q_table, s1, a1, r1, s2, a2, lr, dr):
+    old_q_values, old_frequencies = q(q_table, s1)
+    q1 = q(q_table, s1)[0][a1]
+    q2 = q(q_table, s2)[0][a2]
+    q1 += lr * (r1 + (dr * q2) - q1)
+    old_q_values[a1] = q1
+    old_frequencies[a1] += 1
+    q_table[tuple(s1.tolist())] = [old_q_values, old_frequencies]
 
-def train(number_of_episodes):
+def last_action_update_q(q_table, s1, a1, r1, lr, dr):
+    old_q_values, old_frequencies = q(q_table, s1)
+    old_frequencies[a1] += 1
+    q1 = old_q_values[a1]
+    q1 += lr * (r1 - q1)
+    old_q_values[a1] = q1
+    q_table[tuple(s1.tolist())] = [old_q_values, old_frequencies]
+
+def multiply_actions(s1, a1, r1, s2, a2, r2):
+    '''When you rotate or mirror XOX game, that is also a valid game and
+    action values of rotation and mirroring should also be the same.
+    Using this fact, we can create multiple state, action, reward pairs
+    from a single state, action, reward and use those values in training.'''
+    remap_indices = [
+        np.array([0, 1, 2, 3, 4, 5, 6, 7, 8]),
+        np.array([6, 7, 8, 3, 4, 5, 0, 1, 2]),
+        np.array([2, 1, 0, 5, 4, 3, 8, 7, 6]),
+        np.array([8, 7, 6, 5, 4, 3, 2, 1, 0])
+    ]
+
+    result = {}
+    for remap in remap_indices:
+        sx, ax = s1[remap], remap[a1]
+        sy, ay = s2[remap], remap[a2]
+        key = tuple([*sx.tolist(), ax, *sy.tolist(), ay])
+        if key not in result:
+            result[key] = (sx, ax, r1, sy, ay, r2)
+    return result.values()
+
+
+def train(number_of_episodes, use_model, skip_result, skip_percentage):
     # td(0) updates with Upper Confidence Bound
     dr = 0.99  # discount rate
     lr = 0.01  # learning rate
@@ -76,9 +115,11 @@ def train(number_of_episodes):
     loses = 0
     ties = 0
 
+    ten_percent = number_of_episodes // 10
+
     for i in range(number_of_episodes):
-        if i % 10000 == 0:
-            print(i / number_of_episodes)
+        if not skip_percentage and ((i + 1) % ten_percent == 0 or i == number_of_episodes - 1):
+            print('%.1f%%' % (((i + 1) / number_of_episodes) * 100), file=sys.stderr)
         done = False
         while not done:
             cur_state = xox.state.copy()
@@ -96,23 +137,16 @@ def train(number_of_episodes):
             if len(last_two) == 2:
                 s1, a1, r1 = sarsa_buf[-2]
                 s2, a2, r2 = sarsa_buf[-1]
-                # update according to td(0)
-                old_q_values, old_frequencies = q(q_table, s1)
-                q1 = q(q_table, s1)[0][a1]
-                q2 = q(q_table, s2)[0][a2]
-                q1 += lr * (r1 + (dr * q2) - q1)
-                old_q_values[a1] = q1
-                old_frequencies[a1] += 1
-                q_table[tuple(s1.tolist())] = [old_q_values, old_frequencies]
+
+                tuples = multiply_actions(s1, a1, r1, s2, a2, r2) if use_model else [(s1, a1, r1, s2, a2, r2)]
+                for sarsa in tuples:
+                    update_q(q_table, sarsa[0], sarsa[1], sarsa[2], sarsa[3], sarsa[4], lr, dr)
 
             if done:
                 s1, a1, r1 = sarsa_buf[-1]
-                old_q_values, old_frequencies = q(q_table, s1)
-                old_frequencies[a1] += 1
-                q1 = old_q_values[a1]
-                q1 += lr * (r1 - q1)
-                old_q_values[a1] = q1
-                q_table[tuple(s1.tolist())] = [old_q_values, old_frequencies]
+                tuples = multiply_actions(s1, a1, r1, s1, a1, r1) if use_model else [(s1, a1, r1, s1, a1, r1)]
+                for sarsa in tuples:
+                    last_action_update_q(q_table, sarsa[0], sarsa[1], sarsa[2], lr, dr)
 
                 if r == 1:
                     wins += 1
@@ -123,8 +157,8 @@ def train(number_of_episodes):
         done = False
         xox.reset()
 
-    print(f'q table len: {len(q_table)}')
-    print(f'wins: {wins}, loses: {loses}, ties: {ties}')
+    if not skip_result:
+        print(f'wins: {wins}, loses: {loses}, ties: {ties}')
     with open('q_table.pckl', 'wb') as f:
         pickle.dump(q_table, f)
 
@@ -163,8 +197,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('phase', choices=['train', 'test'])
     parser.add_argument('--episodes', type=int, default=10000)
+    parser.add_argument('--use-model', action='store_true')
+    parser.add_argument('--test-after', type=int, default=0)
+    parser.add_argument('--skip-printing-training', action='store_true')
+    parser.add_argument('--skip-percentage', action='store_true')
     args = parser.parse_args()
     if args.phase == 'train':
-        train(args.episodes)
+        train(args.episodes, args.use_model, args.skip_printing_training, args.skip_percentage)
+        if args.test_after:
+            test(args.test_after)
     else:
         test(args.episodes)
