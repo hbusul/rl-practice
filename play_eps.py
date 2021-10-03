8f5aea7b2373b9a59be761117c1db375fec53725
hbusul <h.burak.usul@gmail.com>
2021-10-03 03:32:39 +0300

    refactor play_eps so we can call it with same args

--- play_eps.py
+++ play_eps.py
@@ -1,70 +1,54 @@
-#!/home/burak/anaconda3/envs/rl/bin/python
-import math
-import gym
-from gym import spaces, logger
-from gym.utils import seeding
+import argparse
+import pickle
+import sys
+
 import numpy as np
 
+from xox_env import XOX
 
-class XOX(gym.Env):
-    '''Some description here'''
 
-    def __init__(self):
-        self.state: np.ndarray = np.array([0] * 9)
-        self.action_space = spaces.Discrete(9)
-        self.observation_space = spaces.MultiDiscrete([3] * 9)
-        self.seed()
-
-    def seed(self, seed=None):
-        self.np_random, seed = seeding.np_random(seed)
-        return [seed]
-
-    def reset(self):
-        self.state: np.ndarray = np.array([0] * 9)
-
-    def step(self, action):
-        err_msg = f'{action} ({action}) invalid'
-        assert self.action_space.contains(action), err_msg
-        assert self.state[action] == 0, 'square was already full'
-
-        # player makes a move
-        self.state[action] = 1
-        s = self.state.reshape(3, 3)
-        if any([((s[i] == 1).all() or (s[:, i] == 1).all()) for i in range(3)]) or (s.diagonal() == 1).all()\
-                or (s[((0, 1, 2), (2, 1, 0))] == 1).all():
-            return self.state, 1, True, {}
-
-        # opponent makes the next move
-        free_places = np.where(self.state == 0)[0]
-        if len(free_places) == 0:
-            return self.state, 0.5, True, {}
-
-        opponent_move = free_places[self.np_random.randint(len(free_places))]
-        self.state[opponent_move] = 2
-        s = self.state.reshape(3, 3)
-        if any([((s[i] == 2).all() or (s[:, i] == 2).all()) for i in range(3)]) or (s.diagonal() == 2).all()\
-                or (s[((0, 1, 2), (2, 1, 0))] == 2).all():
-            reward = -1
-            return self.state, -1, True, {}
-
-        if len(free_places) == 1:
-            return self.state, 0.5, True, {}
-        else:
-            return self.state, 0, False, {}
-
-def q(values, state : np.ndarray):
+def q(values, state: np.ndarray):
     x = tuple(state.tolist())
     if x not in values:
         values[x] = [np.array([0] * 9), np.array([0] * 9)]
     return values[x]
 
+def test(number_of_episodes):
+    '''Play games against agent given number of times and report results'''
+    wins = 0
+    loses = 0
+    ties = 0
 
-def main():
+    xox = XOX()
+    with open('q_table_eps.pckl', 'rb') as f:
+        q_table = pickle.load(f)
+
+    for _ in range(number_of_episodes):
+        done = False
+        while not done:
+            cur_state = xox.state.copy()
+            q_values, _ = q(q_table, cur_state)
+            q_values[cur_state != 0] = -100
+            act = np.argmax(q_values)
+            _, reward, done, _ = xox.step(act)
+
+            if done:
+                if reward == 1:
+                    wins += 1
+                elif reward == -1:
+                    loses += 1
+                else:
+                    ties += 1
+        xox.reset()
+
+    print(f'wins: {wins}, loses: {loses}, ties: {ties}')
+
+
+def train(number_of_episodes, use_model, skip_result, skip_percentage):
     # epsilon-greedy td(0) learning
-    dr = 0.99 # discount rate
-    lr = 0.01 # learning rate
+    dr = 0.99  # discount rate
+    lr = 0.01  # learning rate
 
-    number_of_episodes = 10000
     q_table = {}
     xox = XOX()
 
@@ -73,7 +57,15 @@ def main():
     loses = 0
     ties = 0
 
+    if use_model:
+        # TODO Implement this
+        print('--use-model is not supported yet for epsilon-greedy alg. I will put it soon.', file=sys.stderr)
+
+    ten_percent = number_of_episodes // 10
+
     for i in range(number_of_episodes):
+        if not skip_percentage and ((i + 1) % ten_percent == 0 or i == number_of_episodes - 1):
+            print('%.1f%%' % (((i + 1) / number_of_episodes) * 100), file=sys.stderr)
         epsilon = 1 / (i + 1)
         done = False
         while not done:
@@ -99,9 +91,9 @@ def main():
                 q1 += lr * (r1 + (dr * q2) - q1)
                 old_q_values[a1] = q1
                 q_table[tuple(s1.tolist())] = [old_q_values, old_frequencies]
-            
+
             #last_three = sarsa_buf[-3:]
-            #if len(last_three) == 3:
+            # if len(last_three) == 3:
                 #s1, a1, r1 = sarsa_buf[-3]
                 #_, _, r2 = sarsa_buf[-2]
                 #s3, a3, r3 = sarsa_buf[-1]
@@ -123,8 +115,24 @@ def main():
                     ties += 1
         done = False
         xox.reset()
+    if not skip_result:
+        print(f'wins: {wins}, loses: {loses}, ties: {ties}')
+    with open('q_table_eps.pckl', 'wb') as f:
+        pickle.dump(q_table, f)
 
-    print(f'wins: {wins}, loses: {loses}, ties: {ties}')
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('phase', choices=['train', 'test'])
+    parser.add_argument('--episodes', type=int, default=10000)
+    parser.add_argument('--use-model', action='store_true')
+    parser.add_argument('--test-after', type=int, default=0)
+    parser.add_argument('--skip-printing-training', action='store_true')
+    parser.add_argument('--skip-percentage', action='store_true')
+    args = parser.parse_args()
+    if args.phase == 'train':
+        train(args.episodes, args.use_model, args.skip_printing_training, args.skip_percentage)
+        if args.test_after:
+            test(args.test_after)
+    else:
+        test(args.episodes)
