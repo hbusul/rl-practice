c48bb86056dc2b938a76a8fefaf815e830e7fce3
hbusul <h.burak.usul@gmail.com>
2021-10-03 17:40:34 +0300

    update epsilon-greedy agent so it can also use model and also add option to provide fixed epsilon

--- play_eps.py
+++ play_eps.py
@@ -4,14 +4,9 @@ import sys
 
 import numpy as np
 
+from utils import get_q, update_q, last_action_update_q, multiply_actions
 from xox_env import XOX
 
-
-def q(values, state: np.ndarray):
-    x = tuple(state.tolist())
-    if x not in values:
-        values[x] = [np.array([0] * 9), np.array([0] * 9)]
-    return values[x]
 
 def test(number_of_episodes):
     '''Play games against agent given number of times and report results'''
@@ -27,7 +22,7 @@ def test(number_of_episodes):
         done = False
         while not done:
             cur_state = xox.state.copy()
-            q_values, _ = q(q_table, cur_state)
+            q_values, _ = get_q(q_table, cur_state)
             q_values[cur_state != 0] = -100
             act = np.argmax(q_values)
             _, reward, done, _ = xox.step(act)
@@ -44,7 +39,7 @@ def test(number_of_episodes):
     print(f'wins: {wins}, loses: {loses}, ties: {ties}')
 
 
-def train(number_of_episodes, use_model, skip_result, skip_percentage):
+def train(number_of_episodes, use_model, skip_result, skip_percentage, fixed_epsilon):
     # epsilon-greedy td(0) learning
     dr = 0.99  # discount rate
     lr = 0.01  # learning rate
@@ -57,20 +52,16 @@ def train(number_of_episodes, use_model, skip_result, skip_percentage):
     loses = 0
     ties = 0
 
-    if use_model:
-        # TODO Implement this
-        print('--use-model is not supported yet for epsilon-greedy alg. I will put it soon.', file=sys.stderr)
-
     ten_percent = number_of_episodes // 10
 
     for i in range(number_of_episodes):
         if not skip_percentage and ((i + 1) % ten_percent == 0 or i == number_of_episodes - 1):
             print('%.1f%%' % (((i + 1) / number_of_episodes) * 100), file=sys.stderr)
-        epsilon = 1 / (i + 1)
+        epsilon = 1 / (i + 1) if fixed_epsilon != 0 else fixed_epsilon
         done = False
         while not done:
             cur_state = xox.state.copy()
-            q_values, frequencies = q(q_table, cur_state)
+            q_values, frequencies = get_q(q_table, cur_state)
             rand = xox.np_random.rand()
             free_places = np.where(xox.state == 0)[0]
             if rand < epsilon:
@@ -82,31 +73,19 @@ def train(number_of_episodes, use_model, skip_result, skip_percentage):
             sarsa_buf.append((cur_state, act, r))
             last_two = sarsa_buf[-2:]
             if len(last_two) == 2:
-                s1, a1, r1 = sarsa_buf[-2]
-                s2, a2, r2 = sarsa_buf[-1]
-                # update according to td(0)
-                old_q_values, old_frequencies = q(q_table, s1)
-                q1 = q(q_table, s1)[0][a1]
-                q2 = q(q_table, s2)[0][a2]
-                q1 += lr * (r1 + (dr * q2) - q1)
-                old_q_values[a1] = q1
-                q_table[tuple(s1.tolist())] = [old_q_values, old_frequencies]
+                sar1 = sarsa_buf[-2]
+                sar2 = sarsa_buf[-1]
 
-            #last_three = sarsa_buf[-3:]
-            # if len(last_three) == 3:
-                #s1, a1, r1 = sarsa_buf[-3]
-                #_, _, r2 = sarsa_buf[-2]
-                #s3, a3, r3 = sarsa_buf[-1]
-
-                #old_q_values = q(q_table, s1)
-                #q1 = q(q_table, s1)[a1]
-                #q3 = q(q_table, s3)[a3]
-                #
-                #q1 += lr * (r1 + (dr * (r2 + (dr * q3))) - q1)
-                #old_q_values[a1] = q1
-                #q_table[tuple(s1.tolist())] = old_q_values
+                tuples = multiply_actions(*sar1, *sar2) if use_model else [(*sar1, *sar2)]
+                for sarsa in tuples:
+                    update_q(q_table, sarsa[0], sarsa[1], sarsa[2], sarsa[3], sarsa[4], lr, dr)
 
             if done:
+                sar1 = sarsa_buf[-1]
+                tuples = multiply_actions(*sar1, *sar1) if use_model else [(*sar1, *sar1)]
+                for sarsa in tuples:
+                    last_action_update_q(q_table, sarsa[0], sarsa[1], sarsa[2], lr)
+
                 if r == 1:
                     wins += 1
                 elif r == -1:
@@ -129,9 +108,10 @@ if __name__ == '__main__':
     parser.add_argument('--test-after', type=int, default=0)
     parser.add_argument('--skip-printing-training', action='store_true')
     parser.add_argument('--skip-percentage', action='store_true')
+    parser.add_argument('--fixed-epsilon', type=float, default=0)
     args = parser.parse_args()
     if args.phase == 'train':
-        train(args.episodes, args.use_model, args.skip_printing_training, args.skip_percentage)
+        train(args.episodes, args.use_model, args.skip_printing_training, args.skip_percentage, args.fixed_epsilon)
         if args.test_after:
             test(args.test_after)
     else:
